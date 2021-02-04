43101e45807df2f3c7b65a7c9824afd775fbe828
joona127 <65039165+joona127@users.noreply.github.com>
2021-02-04 20:18:46 +0200

    valmis

--- App.js
+++ App.js
@@ -5,83 +5,66 @@ import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 export default function App() {
 
-  const [location, setLocation] = React.useState('');
-  const [targetLatitude, setTargetLatitude] = React.useState(60.200692);
-  const [targetLongitude, setTargetLongitude] = React.useState(24.934302);
-  const [data, setData] = React.useState([]);
+  const initial = {
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221
+  };
+  
+  const [region, setRegion] = useState(initial);
+  const [address, setAddress] = useState('');
 
-  const fetchData = useCallback(async () => {
-    const url = `https://www.mapquestapi.com/geocoding/v1/address?key=WwGFrdNRZArJkEPUKS6jTezQcrq8KnlK&inFormat=kvp&outFormat=json&location=${location}&thumbMaps=false`;
-    const response = await fetch(url);
-    const responseData = await response.json();
-    if (response.ok) {
-     setData(responseData.results[0].locations[0])
-    } else {
-      console.error('Failed to fetch');
-    }
-     
-  })
-
-  const onRegionChange = () => {
-    return {
-      region: {
-        latitude: targetLatitude,
-        longitude: targetLongitude,
-      },
-    };
-  }
-
-  const getLocation = async () => {
+  useEffect(() => {
+  const fetchLocation = async () => {
     let { status } = await Location.requestPermissionsAsync();
     if (status !== 'granted') {
       Alert.alert('No permission to access location');
 
     } else {
-      let location = await Location.getCurrentPositionAsync({});
+      try {
+      let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
+      console.log(location);
+      setRegion({... region, latitude: location.coords.latitude, longitude: location.coords.longitude});
+      } catch (error) {
+       console.log(error.message);
+      }
     }
-  };
-
-  useEffect (() => {
-    fetchData();
-    getLocation();
-  }, [location]);
-
-  const findLocation = () => {
-   setTargetLatitude(data.latLng.lat);
-    setTargetLongitude(data.latLng.lng);
-    onRegionChange();
+  
   }
+fetchLocation();
+}, []);
 
+   const fetchData = async (address) => {
+    const url = `https://www.mapquestapi.com/geocoding/v1/address?key=WwGFrdNRZArJkEPUKS6jTezQcrq8KnlK&inFormat=kvp&outFormat=json&location=${address}&thumbMaps=false`;
+    const response = await fetch(url);
+    const responseData = await response.json();
+    if (response.ok) {
+      const { lat, lng } = responseData.results[0].locations[0].latLng;
+     setRegion({...region, latitude: lat, longitude: lng})
+    } else {
+      console.error('Failed to fetch');
+    }
+     
+  }
 
   return (
     <View style= {{ flex: 1}}>
     <MapView
     style={{ flex: 1}}
-    initialRegion={{
-      latitude: getLocation(),
-      longitude: getLocation(),
-      latitudeDelta: 0.0322,
-      longitudeDelta: 0.0221,
-    }} 
-    region={{
-      latitude: targetLatitude,
-      longitude: targetLongitude,
-      latitudeDelta: 0.1,
-      longitudeDelta: 0.2,
-    }}>
+    region={region} 
+    >
     <Marker
-    coordinate={{
-      latitude: targetLatitude,
-      longitude: targetLongitude,}}
-      title={location} />
+    coordinate={region} />
       </MapView> 
       <TextInput
       style={styles.input}
-      value= { location }
-      onChangeText={(location) => setLocation(location)}
+      placeholder={'Address'}
+      value= { address }
+      onChangeText={text => setAddress(text)}
       />
       <View style={styles.button}>
-      <Button title="Show" onPress={() => findLocation()} />
+      <Button title="Show" onPress={() => fetchData(address)} />
       </View>
       </View>
   );
